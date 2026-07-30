aa2c49895f95968996efc3659d6df7991977ce83
bunhounuy-cpu <bunhou.nuy@gmail.com>
2026-07-30 19:25:45 +0700

    change api

--- frontend/src/api/client.js
+++ frontend/src/api/client.js
@@ -1,4 +1,4 @@
-const API_URL = 'http://localhost:3000/api';
+const API_URL = 'https://course-management-838s.onrender.com/api';
 
 async function request(endpoint, options = {}) {
   const { method = 'GET', body, auth = false } = options;
